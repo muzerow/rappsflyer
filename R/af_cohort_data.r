f1ed74c480f90854c456a6650391c91dc28abc02
muzerow <muzerow6@gmail.com>
2022-08-02 17:12:48 +0300

    Initial commit

---
+++ R/af_cohort_data.r
@@ -0,0 +1,46 @@
+#' Get Data from AppsFlyer Cohort API
+#' @description Get Data from AppsFlyer Cohort API <https://support.appsflyer.com/hc/en-us/articles/360004799057-Cohort-API>
+#' @importFrom httr add_headers content POST
+#' @importFrom jsonlite toJSON
+#' @importFrom stringr str_glue
+#'
+#' @param app_id AppsFlyer App ID
+#' @param cohort_type Cohort attribution (conversion) type
+#' @param min_cohort_size Minimum cohort size
+#' @param from Lower bound of the LTV attribution date range
+#' @param to Higher bound of the LTV attribution date range
+#' @param granularity Data granularity
+#' @param partial_data Partial data
+#' @param filters Filters
+#' @param groupings Groupings
+#' @param kpis Selected KPI
+#' @param preferred_currency KPI revenue currency
+#' @param preferred_timezone Time zone of data ranges
+#' @param aggregation_type Aggregation type
+#' @param per_user Data per user
+#' @param signature Signature
+#'
+#' @export
+
+af_cohort_data <- function(app_id, cohort_type, min_cohort_size = 1, from, to, granularity = "day", partial_data = T, filters = NULL, groupings,
+                           kpis, preferred_currency = F, preferred_timezone = F, aggregation_type, per_user = F, signature) {
+  json_body <- toJSON(list(cohort_type = cohort_type, min_cohort_size = min_cohort_size,
+                           from = from, to = to, granularity = granularity, partial_data = partial_data,
+                           filters = filters, groupings = groupings, kpis = list(kpis),
+                           preferred_currency = preferred_currency, preferred_timezone = preferred_timezone,
+                           aggregation_type = aggregation_type, per_user = per_user),
+                      auto_unbox = T)
+
+  cohort_data <- POST(str_glue("https://hq1.appsflyer.com/api/cohorts/v1/data/app/{app_id}?format=csv"),
+                      body = json_body, encode = "raw",
+                      add_headers("Authorization" = signature,
+                                  "Content-Type" = "application/json",
+                                  "Accept" = "application/json"))
+
+  if (status_code(cohort_data) != 200) {
+    stop(paste0("Error code ", cohort_data$status_code, ": ", content(cohort_data)))
+  }
+
+  content(cohort_data, as = "parsed", type = "text/csv", encoding = "UTF-8")
+}
+
---
+++ R/af_master_data.r
@@ -0,0 +1,38 @@
+#' Get Data from AppsFlyer Master API
+#' @description Get Data from AppsFlyer Master API <https://support.appsflyer.com/hc/en-us/articles/213223166-Master-API-user-acquisition-metrics-via-API>
+#' @importFrom httr content GET
+#'
+#' @param api_token AppsFlyer API token
+#' @param app_id AppsFlyer App ID
+#' @param from Lower bound of the LTV attribution date range
+#' @param to Higher bound of the LTV attribution date range
+#' @param groupings Groupings
+#' @param kpis List of KPIs
+#' @param filters Filters
+#' @param currency KPI revenue currency
+#' @param timezone Time zone of data ranges
+#' @param format CSV or JSON formatting
+#'
+#' @export
+
+af_master_data <- function(api_token = getOption("apps_flyer_api_key"), app_id = getOption("apps_flyer_app_id"),
+                           from, to, groupings, kpis, filters = NULL, currency = NULL, timezone = NULL, format = NULL) {
+  master_data <- GET("https://hq.appsflyer.com/export/master_report/v4",
+                     query = list(api_token = api_token,
+                                  app_id = app_id,
+                                  from = from,
+                                  to = to,
+                                  groupings = str_c(groupings, collapse = ","),
+                                  kpis = str_c(kpis, collapse = ","),
+                                  filters = filters,
+                                  currency = currency,
+                                  timezone = timezone,
+                                  format = format))
+
+  if (status_code(master_data) != 200) {
+    stop(paste0("Error code ", master_data$status_code, ": ", content(master_data)))
+  }
+
+  content(master_data, as = "parsed", type = "text/csv", encoding = "UTF-8")
+}
+
